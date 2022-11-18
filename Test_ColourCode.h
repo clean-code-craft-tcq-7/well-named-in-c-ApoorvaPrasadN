a6dedeea57fe8813b6d87414d680837cc5a8c9bd
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-18 17:14:14 +0530

    Update Test_ColourCode.h

--- Test_ColourCode.h
+++ Test_ColourCode.h
@@ -1,3 +1,4 @@
 
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void Test_PairNumberToColour(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void Test_ColorToPairNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
 
