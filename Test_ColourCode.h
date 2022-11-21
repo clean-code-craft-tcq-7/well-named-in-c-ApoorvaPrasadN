ab234bcec63d0cf47b8ab5e31ec2fcb3ca817af6
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-21 12:38:25 +0530

    Update Test_ColourCode.h

--- Test_ColourCode.h
+++ Test_ColourCode.h
@@ -1,4 +1,27 @@
+/*Function Declaration*/
+extern void Test_PairNumberToColour(int pairNumber,enum MajorColor_List expectedMajor,enum MinorColor_List expectedMinor);
+extern void Test_ColorToPairNumber(enum MajorColor_List major,enum MinorColor_List minor,int expectedPairNumber);
+  
+/*variable Declaration*/
+enum MajorColor_List {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor_List {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-void Test_PairNumberToColour(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-void Test_ColorToPairNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
+const char* MajorColor_Array[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColor_Array) / sizeof(MajorColor_Array[0]);
+int numberOfMinorColors =
+    sizeof(MajorColor_Array) / sizeof(MajorColor_Array[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+typedef struct {
+    enum MajorColor_List majorColor;
+    enum MinorColor_List minorColor;
+} ColorPair;
 
