39c20546ee358060388a9d0edfde5b220856e752
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-21 14:10:21 +0530

    Update Get_ColourPair_PairNumber.c

--- Get_ColourPair_PairNumber.c
+++ Get_ColourPair_PairNumber.c
@@ -1,1 +1,17 @@
+#include "Test_ColourCode.h"
 
+/*This function returns the Color pairs numbers for corresponding pair numbers(given)*/ 
+ColorPair Get_ColorfromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
