314d98b7dc4c6ee3d98485b89ce423b0f6ba0e09
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-21 13:58:19 +0530

    Update Test_ColourPair.c

--- Test_ColourPair.c
+++ Test_ColourPair.c
@@ -1,1 +1,15 @@
+#include "Test_ColourCode.h"
 
+/*This function is used to test the colour from paired number*/
+
+void Test_PairNumberToColour(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
