ae77d77683f272e38bc329f82cf528d08e1ca107
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-23 00:49:52 +0530

    Update Test.h

--- Test.h
+++ Test.h
@@ -2,18 +2,6 @@
 typedef enum {WHITE=0, RED, BLACK,YELLOW, VIOLET}MajorColor_List;
 typedef enum {BLUE=0,ORANGE, GREEN, BROWN, SLATE}MinorColor_List;
 
-const char* MajorColorNames_Array[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-const char* MinorColorNames_Array[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-int numberOfMajorColors =
-    sizeof(MajorColorNames_Array) / sizeof(MajorColorNames_Array[0]);
-int numberOfMinorColors =
-    sizeof(MinorColorNames_Array) / sizeof(MinorColorNames_Array[0]);
-
 typedef struct {
     MajorColor_List majorColor;
     MinorColor_List minorColor;
