9f3b95ce1348ef973c49751da962eff8fc079d14
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 17:23:59 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 /*This function is used to decode the pairnumber into wirecolours and vice-versa*/
 #include <stdio.h>
 #include <assert.h>
-#include "Test_ColourCode.h"
 
 int main() {
-    Test_PairNumberToColorPair(4, (MajorColor_List)WHITE, (MinorColor_List)BROWN);
-    Test_PairNumberToColorPair(5, (MajorColor_List)WHITE, (MinorColor_List)SLATE);
+    Test_PairNumberToColorPair(4, WHITE, BROWN);
+    Test_PairNumberToColorPair(5, WHITE, SLATE);
 
-    Test_ColorPairToPairNumber((MajorColor_List)BLACK, (MinorColor_List)ORANGE, 12);
-    Test_ColorPairToPairNumber((MajorColor_List)VIOLET,(MinorColor_List)SLATE, 25);
+    Test_ColorPairToPairNumber(BLACK,ORANGE, 12);
+    Test_ColorPairToPairNumber(VIOLET,SLATE, 25);
 
     return 0;
 }
