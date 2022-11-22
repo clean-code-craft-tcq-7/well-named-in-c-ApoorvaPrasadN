1fbf4284addc62507bedfc072f604b18f8f57968
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 13:10:35 +0530

    Update Test_PairNumber.c

--- Test_PairNumber.c
+++ Test_PairNumber.c
@@ -3,8 +3,8 @@
 /*This function is used to test the paired number from colour pairs*/
 
 void Test_ColorPairToPairNumber(
-    enum MajorColor_List  major,
-    enum MinorColor_List minor,
+    MajorColor_List major,
+    MinorColor_List minor,
     int expectedPairNumber)
 {
     ColorPair colorPair;
