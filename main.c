044f5d3243a562c8735fbacd0c7ed935c973aa8c
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 15:23:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 
 int main() {
     Test_PairNumberToColorPair(4, (MajorColor_List)WHITE, (MinorColor_List)BROWN);
-    Test_PairNumberToColorPair(5, WHITE, SLATE);
+    Test_PairNumberToColorPair(5, (MajorColor_List)WHITE, (MinorColor_List)SLATE);
 
-    Test_ColorPairToPairNumber(BLACK, ORANGE, 12);
-    Test_ColorPairToPairNumber(VIOLET, SLATE, 25);
+    Test_ColorPairToPairNumber((MajorColor_List)BLACK, (MinorColor_List)ORANGE, 12);
+    Test_ColorPairToPairNumber((MajorColor_List)VIOLET,(MinorColor_List)SLATE, 25);
 
     return 0;
 }
