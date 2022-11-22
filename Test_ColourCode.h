d2d299e0c3354e5d9499dd5682981c4904771d07
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 13:44:04 +0530

    Update Test_ColourCode.h

--- Test_ColourCode.h
+++ Test_ColourCode.h
@@ -25,7 +25,7 @@ int numberOfMinorColors =
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 typedef struct {
-    MajorColor_List majorColor;
-    MinorColor_List minorColor;
+    enum MajorColor_List majorColor;
+    enum MinorColor_List minorColor;
 } ColorPair;
 
