045b64b1b9c3ee3dc51410abac777bc60b5214a3
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-23 01:08:16 +0530

    Update Test_ColorPair.c

--- Test_ColorPair.c
+++ Test_ColorPair.c
@@ -1,13 +1,15 @@
 #include "Test.h"
 #include <stdio.h>
 #include <assert.h>
-
+const int MAX_COLORPAIR_NAME_CHAR = 16;
 void Test_ColorPairToPairNumber(MajorColor_List major,MinorColor_List minor,int expectedPairNumber)
 {
     ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = Get_PairNumberFromColor(&colorPair);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHAR];
+    Mapping_StringToColorPair(colorPairNames,&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);  
 }
