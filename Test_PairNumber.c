8c038b8eaa5a20b7146aefbb1ce1cf014de77e0c
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 20:43:10 +0530

    Create Test_PairNumber.c

---
+++ Test_PairNumber.c
@@ -0,0 +1,11 @@
+#include "Test.h"
+
+void Test_PairNumberToColorPair(int pairNumber,MajorColor_List expectedMajor,MinorColor_List expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor); 
+}
