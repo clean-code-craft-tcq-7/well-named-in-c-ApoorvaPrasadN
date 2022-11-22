305a5ae7e12ba5ed437f467602fc95709fef63e8
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 17:25:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #include <assert.h>
 
 int main() {
-    Test_PairNumberToColorPair(4, WHITE, BROWN);
-    Test_PairNumberToColorPair(5, WHITE, SLATE);
-
-    Test_ColorPairToPairNumber(BLACK,ORANGE, 12);
-    Test_ColorPairToPairNumber(VIOLET,SLATE, 25);
-
+    Test_PairNumberToColorPair(WHITE);
     return 0;
 }
