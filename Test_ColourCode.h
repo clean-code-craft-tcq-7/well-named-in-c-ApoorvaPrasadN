101d320db274e868ef1e2d0b32a1e552a4194145
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 12:50:25 +0530

    Update Test_ColourCode.h

--- Test_ColourCode.h
+++ Test_ColourCode.h
@@ -10,17 +10,17 @@ extern void Mapping_StringToColorPair(char* buffer,const ColorPair* colorPair);
 enum MajorColor_List {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor_List {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColor_Array[] = {
+const char* MajorColorNames_Array[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-const char* MinorColorNames[] = {
+const char* MinorColorNames_Array[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
 int numberOfMajorColors =
-    sizeof(MajorColor_Array) / sizeof(MajorColor_Array[0]);
+    sizeof(MajorColorNames_Array) / sizeof(MajorColorNames_Array[0]);
 int numberOfMinorColors =
-    sizeof(MajorColor_Array) / sizeof(MajorColor_Array[0]);
+    sizeof(MinorColorNames_Array) / sizeof(MinorColorNames_Array[0]);
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
