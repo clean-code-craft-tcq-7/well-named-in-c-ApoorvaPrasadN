8614cf641d4ca63d27ae56ff0ae54622e766dccf
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-23 00:50:10 +0530

    Update Test2.h

--- Test2.h
+++ Test2.h
@@ -1,1 +1,11 @@
+const char* MajorColorNames_Array[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+const char* MinorColorNames_Array[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
 
+int numberOfMajorColors =
+    sizeof(MajorColorNames_Array) / sizeof(MajorColorNames_Array[0]);
+int numberOfMinorColors =
+    sizeof(MinorColorNames_Array) / sizeof(MinorColorNames_Array[0]);
