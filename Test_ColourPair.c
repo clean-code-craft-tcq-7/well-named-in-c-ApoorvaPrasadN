36a7157e4841656dba64dab13f18e8284ca5dc79
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-22 13:10:10 +0530

    Update Test_ColourPair.c

--- Test_ColourPair.c
+++ Test_ColourPair.c
@@ -3,8 +3,8 @@
 /*This function is used to test the colour pairs from paired number*/
 
 void Test_PairNumberToColorPair(int pairNumber,
-    enum MajorColor_List expectedMajor,
-    enum MinorColor_List expectedMinor)
+    MajorColor_List expectedMajor,
+    MinorColor_List expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
