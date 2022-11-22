c3861c68fdfd1cd3b0b3d3a0050e4aaf3b3a1c9c
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-23 00:46:31 +0530

    Update Test_PairNumber.c

--- Test_PairNumber.c
+++ Test_PairNumber.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "Test.h"
-
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 void Test_PairNumberToColorPair(int pairNumber,MajorColor_List expectedMajor,MinorColor_List expectedMinor)
 {
     ColorPair colorPairs = Get_ColorFromPairNumber(pairNumber);
