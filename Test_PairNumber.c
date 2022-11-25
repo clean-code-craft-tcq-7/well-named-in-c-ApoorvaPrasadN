6bd8682222e2a522adb6142064b6b3cc4a183e63
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-25 11:57:43 +0530

    Update Test_PairNumber.c

--- Test_PairNumber.c
+++ Test_PairNumber.c
@@ -4,7 +4,7 @@
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 void Test_PairNumberToColorPair(int pairNumber,MajorColor_List expectedMajor,MinorColor_List expectedMinor)
 {
-    ColorPair colorPairs = Get_ColorFromPairNumber(pairNumber);
+    ColorPair colorPair = Get_ColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     Mapping_ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
