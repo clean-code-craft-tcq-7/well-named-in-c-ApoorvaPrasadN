bbad2fa11d2039efef0b9f3156f60753fc6a11bb
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-11-25 11:53:50 +0530

    Update Test_ColorPair.c

--- Test_ColorPair.c
+++ Test_ColorPair.c
@@ -7,7 +7,7 @@ void Test_ColorPairToPairNumber(MajorColor_List major,MinorColor_List minor,int 
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = Get_PairNumberFromColor(&colorPair);
-    Mapping_StringToColorPair(char* buffer,const ColorPair* colorPair);
-    printf("Got pair number %d\n", pairNumber);
+    Mapping_StringToColorPair(&colorPairNames,colorPair);
+    printf("Got pair number %d\n", colorPair);
     assert(pairNumber == expectedPairNumber);  
 }
